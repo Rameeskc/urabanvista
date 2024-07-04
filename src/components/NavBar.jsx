2666f9b162971874fe6bb7817a1a0d3cae3f59fb
[200~Rameeskc <rameeskc72@gmail.com>
2024-07-04 13:17:27 +0530

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,66 @@
 import React,{useState} from 'react'
 import { IoIosArrowDown } from "react-icons/io";
 import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
 
   const [nav, setNav] = useState(false);
-  const handleNav = () => {
-    setNav(!nav)
-  }
+
+
+  const items =[
+    {
+      id:1,
+      item:"Buy"
+    },
+    {
+      id:2,
+      item:"Rent"
+    },
+    {
+      id:3,
+      item:"Sell"
+    },
+    {
+      id:4,
+      item:"Help"
+    },
+
+  ]
 
   return (
-    <div className='h-[80px] w-full bg-blue-800 flex items-center justify-between px-3'>
+    <div className='h-[80px] w-full bg-blue-800 flex items-center justify-between px-3 pr-5'>
       <div className='px-2 w-[150px] lg:w-[300px] lg:px-6 text-white'>
-        <p className='text-xl lg:text-3xl font-Vollkorn font-bold'>URBANVISTA</p>
+        <p className='text-3xl font-Vollkorn font-bold'>URBANVISTA</p>
       </div>
       
-      <ul className='hidden md:flex  px-4 text-white'>
-        <li className=' px-6 flex items-center gap-2'>Buy <IoIosArrowDown /> </li>
-        <li className='px-6 flex items-center gap-2'>Rent <IoIosArrowDown /> </li>
-        <li className='px-6 flex items-center gap-2'>Sell <IoIosArrowDown /> </li>
-        <li className='px-6 flex items-center gap-2'>Help <IoIosArrowDown /> </li>
-      </ul>
+      <ul className='hidden xl:flex  px-4 text-white'>
 
-      <div>
-        <FaBars/>
+        {
+          items.map(({item,id})=>(
+            <li key={id} className=' px-4 flex items-center gap-1'>{item} <IoIosArrowDown /> </li>
+          )
+          )
+        }
+      </ul>
+      <nav className='relative'>
+      <div
+          onClick={()=> setNav(!nav)} 
+          className=' text-white xl:hidden'>
+          {nav? <FaTimes  size={30}/> :<FaBars size={30}/>}  
+        
       </div>
-   
-      
-     
-      {/* <div >
-      <ul className=' flex  px-4 text-white'>
-        <li className=' px-6 flex items-center gap-2'>Buy <IoIosArrowDown /> </li>
-        <li className='px-6 flex items-center gap-2'>Rent <IoIosArrowDown /> </li>
-        <li className='px-6 flex items-center gap-2'>Sell <IoIosArrowDown /> </li>
-        <li className='px-6 flex items-center gap-2'>Help <IoIosArrowDown /> </li>
-      </ul>
 
-      </div> */}
+      { nav && (
+        <ul className='flex flex-col justify-evenly items-center absolute top-[55px] right-[-21px] h-[300px] w-[250px] duration-300 bg-blue-800 xl:hidden'>
+          {
+          items.map(({item,id})=>(
+            <li key={id} className='h-[75px] w-full px-4 flex items-center justify-center gap-1 border'>{item} <IoIosArrowDown /> </li>
+          ))}
+        </ul>
+      )}
+
+    </nav>
     </div>
 
   )
